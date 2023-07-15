98bb7804c8e75e55c7cb45c30783ca4082d12beb
Ardasher <misterardasher@gmail.com>
2023-07-15 17:43:33 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Switcher from './components/Switcher';
+import { BrowserRouter as Router, createBrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom';
+import Layout from './layout/Layout'
+import Home from './pages/Home/Home'
+import SignUp from './pages/SignUp/SignUp';
+import Login from './pages/Login/Login';
+
+
+const router = createBrowserRouter([
+   {
+    path: "signUp",
+    element:  <SignUp/> 
+  },
+  {
+    path: "login",
+    element:<Login/> 
+  },
+   {
+    path: "/",
+    element: <Layout/> ,
+    children: [
+      {
+        index:true,
+        element:  <Home/> 
+      },
+
+      {
+        path: "message",
+        // element: <Message/>
+      },
+
+      {
+        path: "add",
+        // element:  <Add/> 
+      },
+
+      {
+        path: "top",
+        // element:  <Top/>
+      },
+
+      {
+        path: "like",
+        // element: <Like/> 
+      },
+      {
+        path: "profile",
+        // element: <Profile/> 
+      },
+      {
+        path: "editProfile",
+        // element:  <EditProfile/> 
+      },
+      {
+        path: "editPassword",
+        // element: <EditPassword/> 
+      },
+      {
+        path: "direct",
+        // element: <Direct/>
+      }
+    ]
+  }
+   
+  
+])
+
+
+
+const App = () => {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
+
+  return (
+    <div>
+      <RouterProvider router={router}/>
+      {/* <Blog/> */}
+      {/* <div className="w-full flex justify-center">
+        <select onChange={(e) => changeLanguage(e.target.value)}>
+          <option value="en">En</option>
+          <option value="ru">Ru</option>
+        </select>
+      </div> */}
+      {/* <div className="dark:bg-black"> */}
+        {/* <Switcher /> 939401739*/}
+      {/* </div> */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='md:pt-[14%] h-[180vh] w-full bg-[grey] text-[50px]'>Home</div>
+  )
+}
+
+export default Home
